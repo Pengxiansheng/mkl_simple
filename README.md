3ee744fa8ef8f7e56a4df844ace4a8e993896630
Pengxiansheng <358799851@qq.com>
2020-11-06 15:13:36 +0800

    README

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 README
 ===========================
-#测试结果
- 
-加速结果：500维稠密矩阵inverse()，无加速1126ms，加速82ms
-			    llt().solveInPlace(),无加速831ms，有加速8ms
-		2000维稠密矩阵inverse()，无加速65371ms，加速415ms
-			    llt().solveInPlace(),无加速48863ms，有加速857ms 
+##测试结果
+NXN维随机矩阵求逆
+| |*100X100*|*500X500*|*2000X2000*|
+|*llt().solveInPlace()*|9ms|812ms|48863ms|
+|*inverse()*|13ms|1100ms|65371ms|
+|*MKL llt().solveInPlace()*|*4ms*|*8ms*|*414ms*|
+|*MKL inverse()*|71ms|91ms|870ms|
 ===========================		
 ## 注
-
-1.线程看空闲情况自动分配，可能全部都占（估计学习MKL可以控制）  
-2.另外也可以考虑用float型，时间会进一步减少
+* #define EIGEN_USE_MKL_ALL 需要写在Eigen头文件前
+* llt().solveInPlace() 求逆结果有可能会与原矩阵相乘不为单位正，可以通过在对角元素上加小数解决
+* 线程看空闲情况自动分配，可能全部都占（估计学习MKL可以控制）  
+* 另外也可以考虑用float型，时间会进一步减少
--- test.cpp
+++ test.cpp
@@ -1,18 +1,17 @@
+#define EIGEN_USE_MKL_ALL
+
 #include <stdio.h>
 #include <Eigen/Dense>
 #include <iomanip>
 #include <iostream>
 
-#define EIGEN_USE_MKL_ALL
-
 using namespace std;
-
 
 int main(int argc, char *argv[]) {
   
-    Eigen::MatrixXd P(130, 130); 
-    P = Eigen::MatrixXd::Random(130,130);
-    Eigen::MatrixXd Pinv_(130, 130);
+    Eigen::MatrixXd P(100, 100); 
+    P = Eigen::MatrixXd::Random(100, 100);
+    Eigen::MatrixXd Pinv_(100, 100);
     Pinv_.setIdentity();
 
     clock_t start;  
@@ -22,13 +21,13 @@ int main(int argc, char *argv[]) {
     P.llt().solveInPlace(Pinv_);
     //cout << P * Pinv_ << endl; // 出来有可能不是单位阵，可以找我交流来修改
     _time = (double)(clock() - start) / CLOCKS_PER_SEC;
-    cout << _time * 1000 << "ms" << endl;  
+    cout << "llt:     " << _time * 1000 << "ms" << endl;  
     cout << "=================" << endl;
     start = clock(); 
     Pinv_ = P.inverse();
     //cout << P * Pinv_ << endl;
     _time = (double)(clock() - start) / CLOCKS_PER_SEC;  
-    cout << _time * 1000 << "ms" << endl;
+    cout << "inverse: " << _time * 1000 << "ms" << endl;
     cout << "=================" << endl;
 
 }
